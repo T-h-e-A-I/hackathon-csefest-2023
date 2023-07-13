bb87e79c539ceef3459e655c9ddec676d0768c06
Jakaria44 <mdjakaria442020@gmail.com>
2023-07-13 10:45:00 +0600

    made ready to deploy

--- nft-shopping/hardhat.config.js
+++ nft-shopping/hardhat.config.js
@@ -2,42 +2,51 @@ require("@matterlabs/hardhat-zksync-solc");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
+zksolc: {
     version: "1.3.9",
     compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
+    defaultNetwork : "sepolia",
+    networks: {
+    hardhat: {},
+    sepolia : {
+        url : "https://rpc.sepolia.dev",
+        accounts : [`0x${process.env.PRIVATE_KEY}`],
+    }
     },
-  },
-  networks: {
+    settings: {
+    optimizer: {
+        enabled: true,
+    },
+    },
+},
+networks: {
     zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
+    url: "https://zksync2-testnet.zksync.dev",
+    ethNetwork: "goerli",
+    chainId: 280,
+    zksync: true,
     },
     zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
+    url: "https://zksync2-mainnet.zksync.io/",
+    ethNetwork: "mainnet",
+    chainId: 324,
+    zksync: true,
     },
-  },
-  paths: {
+},
+paths: {
     artifacts: "./artifacts-zk",
     cache: "./cache-zk",
     sources: "./contracts",
     tests: "./test",
-  },
-  solidity: {
+},
+solidity: {
     version: "0.8.17",
     settings: {
-      optimizer: {
+    optimizer: {
         enabled: true,
         runs: 200,
-      },
     },
-  },
+    },
+},
 };
+
